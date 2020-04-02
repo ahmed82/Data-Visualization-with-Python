b369833b4b8d11ceaa5467a7089484fdcd3b43f3
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-04-02 15:15:51 -0400

    compleate fist section

--- Matplotlib-baisc.py
+++ Matplotlib-baisc.py
@@ -153,21 +153,198 @@ For example, when we are referencing the year 2013, one might confuse that when 
 To avoid this ambuigity, let's convert the column names into strings: '1980' to '2013'."""
 
 df_can.columns = list(map(str, df_can.columns))
-# [print (type(x)) for x in df_can.columns.values] #<-- uncomment to check type of column headers
+[print (type(x)) for x in df_can.columns.values] #<-- uncomment to check type of column headers
 
+"""Since we converted the years to string, let's declare a variable that will allow us to easily call upon the full range of years:"""
+# useful for plotting later on
+years = list(map(str, range(1980, 2014)))
+years
 
+""" ########################## Filtering based on a criteria
+To filter the dataframe based on a condition, we simply pass the condition as a boolean vector.
 
+For example, Let's filter the dataframe to show the data on Asian countries (AreaName = Asia)."""
 
+# 1. create the condition boolean series
+condition = df_can['Continent'] == 'Asia'
+print(condition)
 
+# we can pass mutliple criteria in the same line. 
+# let's filter for AreaNAme = Asia and RegName = Southern Asia
 
+df_can[(df_can['Continent']=='Asia') & (df_can['Region']=='Southern Asia')]
 
+# note: When using 'and' and 'or' operators, pandas requires we use '&' and '|' instead of 'and' and 'or'
+# don't forget to enclose the two conditions in parentheses
 
+# Before we proceed: let's review the changes we have made to our dataframe.
+print('data dimensions:', df_can.shape)
+print(df_can.columns)
+df_can.head(2)
 
+""" ##################### Visualizing Data using Matplotlib #############################
+Matplotlib: Standard Python Visualization Library
+The primary plotting library we will explore in the course is Matplotlib. As mentioned on their website:
 
+Matplotlib is a Python 2D plotting library which produces publication quality figures in a variety of hardcopy formats and interactive environments across platforms. Matplotlib can be used in Python scripts, the Python and IPython shell, the jupyter notebook, web application servers, and four graphical user interface toolkits.
 
+If you are aspiring to create impactful visualization with python, Matplotlib is an essential tool to have at your disposal.
 
+Matplotlib.Pyplot
+One of the core aspects of Matplotlib is matplotlib.pyplot. It is Matplotlib's scripting layer which we studied in details in the videos about Matplotlib. Recall that it is a collection of command style functions that make Matplotlib work like MATLAB. Each pyplot function makes some change to a figure: e.g., creates a figure, creates a plotting area in a figure, plots some lines in a plotting area, decorates the plot with labels, etc. In this lab, we will work with the scripting layer to learn how to generate line plots. In future labs, we will get to work with the Artist layer as well to experiment first hand how it differs from the scripting layer.
 
+Let's start by importing Matplotlib and Matplotlib.pyplot as follows:"""
 
+# we are using the inline backend
+%matplotlib inline 
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# *optional: check if Matplotlib is loaded.
+print ('Matplotlib version: ', mpl.__version__) # >= 2.0.0
+
+""" ############################## Plotting in pandas ###################
+Fortunately, pandas has a built-in implementation of Matplotlib that we can use. 
+Plotting in pandas is as simple as appending a .plot() method to a series or dataframe.
+
+Documentation:
+
+Plotting with Series
+Plotting with Dataframes
+Line Pots (Series/Dataframe) 
+What is a line plot and why use it?
+
+A line chart or line plot is a type of plot which displays information as a series of data points called 'markers' connected by straight line segments. It is a basic type of chart common in many fields. Use line plot when you have a continuous data set. These are best suited for trend-based visualizations of data over a period of time.
+
+Let's start with a case study:
+
+In 2010, Haiti suffered a catastrophic magnitude 7.0 earthquake. The quake caused widespread devastation and loss of life and aout three million people were affected by this natural disaster. As part of Canada's humanitarian effort, the Government of Canada stepped up its effort in accepting refugees from Haiti. We can quickly visualize this effort using a Line plot:
+
+Question: Plot a line graph of immigration from Haiti using df.plot().
+
+First, we will extract the data series for Haiti."""
+
+haiti = df_can.loc['Haiti', years] # passing in years 1980 - 2013 to exclude the 'total' column
+haiti.head()
+
+# Next, we will plot a line plot by appending `.plot()` to the `haiti` dataframe.
+haiti.plot()
+
+"""pandas automatically populated the x-axis with the index values (years), and the y-axis with the column values (population). However, 
+notice how the years were not displayed because they are of type string.Therefore, let's change the type of the index values to integer for plotting.
+
+Also, let's label the x and y axis using plt.title(), plt.ylabel(), and plt.xlabel() as follows:"""
+
+haiti.index = haiti.index.map(int) # let's change the index values of Haiti to type integer for plotting
+haiti.plot(kind='line')
+
+plt.title('Immigration from Haiti')
+plt.ylabel('Number of immigrants')
+plt.xlabel('Years')
+
+plt.show() # need this line to show the updates made to the figure
+
+"""We can clearly notice how number of immigrants from Haiti spiked up from 2010 as Canada stepped up its efforts to accept refugees from Haiti. Let's annotate this spike in the plot by using the plt.text() method."""
+
+haiti.plot(kind='line')
+
+plt.title('Immigration from Haiti')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+# annotate the 2010 Earthquake. 
+# syntax: plt.text(x, y, label)
+plt.text(2000, 6000, '2010 Earthquake') # see note below
+
+plt.show() 
+
+"""With just a few lines of code, you were able to quickly identify and visualize the spike in immigration!
+
+Quick note on x and y values in plt.text(x, y, label):
+
+ Since the x-axis (years) is type 'integer', we specified x as a year. The y axis (number of immigrants) is type 'integer', so we can just specify the value y = 6000.
+    plt.text(2000, 6000, '2010 Earthquake') # years stored as type int
+If the years were stored as type 'string', we would need to specify x as the index position of the year. Eg 20th index is year 2000 since it is the 20th year with a base year of 1980.
+    plt.text(20, 6000, '2010 Earthquake') # years stored as type int
+We will cover advanced annotation methods in later modules.
+We can easily add more countries to line plot to make meaningful comparisons immigration from different countries.
+
+Question: Let's compare the number of immigrants from India and China from 1980 to 2013.
+"""
+# Step 1: Get the data set for China and India, and display dataframe.
+df_CI = df_can.loc[['India', 'China'], years]
+df_CI.head()
+
+# Step 2: Plot graph. We will explicitly specify line plot by passing in kind parameter to plot().        
+df_CI.plot(kind='line')
+
+"""That doesn't look right...
+
+Recall that pandas plots the indices on the x-axis and the columns as individual lines on the y-axis. Since df_CI is a dataframe with the country as the index and years as the columns, we must first transpose the dataframe using transpose() method to swap the row and columns.
+"""
+df_CI = df_CI.transpose()
+df_CI.head()
+
+"""pandas will auomatically graph the two countries on the same graph. Go ahead and plot the new transposed dataframe. Make sure to add a title to the plot and label the axes."""
+
+df_CI.index = df_CI.index.map(int) # let's change the index values of df_CI to type integer for plotting
+df_CI.plot(kind='line')
+
+plt.title('Immigrants from China and India')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+"""From the above plot, we can observe that the China and India have very similar immigration trends through the years.
+
+Note: How come we didn't need to transpose Haiti's dataframe before plotting (like we did for df_CI)?
+
+That's because haiti is a series as opposed to a dataframe, and has the years as its indices as shown below.
+"""
+print(type(haiti))
+print(haiti.head(5))
+
+"""
+Line plot is a handy tool to display several dependent variables against one independent variable. However, it is recommended that no more than 5-10 lines on a single graph; any more than that and it becomes difficult to interpret.
+
+Question: Compare the trend of top 5 countries that contributed the most to immigration to Canada."""
+# The correct answer is:
+# Step 1: Get the dataset. Recall that we created a Total column that calculates the cumulative immigration by country. 
+# We will sort on this column to get our top 5 countries using pandas sort_values() method.
+# inplace = True paramemter saves the changes to the original df_can dataframe
+df_can.sort_values(by='Total', ascending=False, axis=0, inplace=True)
+
+# get the top 5 entries
+df_top5 = df_can.head(5)
+
+# transpose the dataframe
+df_top5 = df_top5[years].transpose() 
+
+print(df_top5)
+
+# Step 2: Plot the dataframe. To make the plot more readeable, we will change the size using the `figsize` parameter.
+df_top5.index = df_top5.index.map(int) # let's change the index values of df_top5 to type integer for plotting
+df_top5.plot(kind='line', figsize=(14, 8)) # pass a tuple (x, y) size
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+plt.show()
+
+"""Other Plots
+Congratulations! you have learned how to wrangle data with python and create a line plot with Matplotlib. There are many other plotting styles available other than the default Line plot, all of which can be accessed by passing kind keyword to plot(). The full list of available plots are as follows:
+
+bar for vertical bar plots
+barh for horizontal bar plots
+hist for histogram
+box for boxplot
+kde or density for density plots
+area for area plots
+pie for pie plots
+scatter for scatter plots
+hexbin for hexbin plot"""
 
 
 
