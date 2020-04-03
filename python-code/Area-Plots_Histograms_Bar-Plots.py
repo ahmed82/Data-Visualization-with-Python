dff81f9dad7d694c81e52ab3aa093688aef75127
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-04-02 22:20:55 -0400

    work on Histgram

--- python-code/Area-Plots_Histograms_Bar-Plots.py
+++ python-code/Area-Plots_Histograms_Bar-Plots.py
@@ -17,3 +17,280 @@ df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-cou
                       )
 
 print('Data downloaded and read into a dataframe!')
+
+df_can.head()
+
+# print the dimensions of the dataframe
+print(df_can.shape)
+
+"""
+1. Clean up the dataset to remove columns that are not informative to us 
+for visualization (eg. Type, AREA, REG)."""
+df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+"""2. Rename some of the columns so that they make sense."""
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace=True)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+"""3. For consistency, ensure that all column labels of type string."""
+# let's examine the types of the column labels
+all(isinstance(column, str) for column in df_can.columns)
+"""Notice how the above line of code returned False when we tested if all the 
+column labels are of type string. So let's change them all to string type."""
+df_can.columns = list(map(str, df_can.columns))
+
+# let's check the column labels types now
+all(isinstance(column, str) for column in df_can.columns)
+
+"""4. Set the country name as index - useful for quickly looking up countries using .loc method."""
+df_can.set_index('Country', inplace=True)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+"""5. Add total column."""
+df_can['Total'] = df_can.sum(axis=1)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+print ('data dimensions:', df_can.shape)
+
+# finally, let's create a list of years from 1980 - 2013
+# this will come in handy when we start plotting the data
+years = list(map(str, range(1980, 2014)))
+
+years
+
+""" ############### Visualizing Data using Matplotlib #############"""
+# use the inline backend to generate the plots within the browser
+# %matplotlib inline 
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.style.use('ggplot') # optional: for ggplot-like style
+
+# check for latest version of Matplotlib
+print ('Matplotlib version: ', mpl.__version__) # >= 3.1.3
+
+"""Area Plots
+In the last module, we created a line plot that visualized the top 5 countries 
+that contribued the most immigrants to Canada from 1980 to 2013. With a little 
+modification to the code, we can visualize this plot as a cumulative plot, 
+also knows as a Stacked Line Plot or Area plot."""
+
+df_can.sort_values(['Total'], ascending=False, axis=0, inplace=True)
+
+# get the top 5 entries
+df_top5 = df_can.head()
+
+# transpose the dataframe
+df_top5 = df_top5[years].transpose() 
+
+df_top5.head()
+
+"""Area plots are stacked by default. And to produce a stacked area plot, each
+ column must be either all positive or all negative values (any NaN values will 
+ defaulted to 0). To produce an unstacked plot, pass stacked=False."""
+df_top5.index = df_top5.index.map(int) # let's change the index values of df_top5 to type integer for plotting
+df_top5.plot(kind='area', 
+             stacked=False,
+             figsize=(20, 10), # pass a tuple (x, y) size
+             )
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+"""The unstacked plot has a default transparency (alpha value) at 0.5. 
+We can modify this value by passing in the alpha parameter."""
+df_top5.plot(kind='area', 
+             alpha=0.25, # 0-1, default value a= 0.5
+             stacked=False,
+             figsize=(20, 10),
+            )
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+"""Two types of plotting
+As we discussed in the video lectures, there are two styles/options of ploting with matplotlib. Plotting using the Artist layer and plotting using the scripting layer.
+
+*Option 1: Scripting layer (procedural method) - using matplotlib.pyplot as 'plt' *
+
+You can use plt i.e. matplotlib.pyplot and add more elements by calling different methods procedurally; for example, plt.title(...) to add title or plt.xlabel(...) to add label to the x-axis.
+
+    # Option 1: This is what we have been using so far
+    df_top5.plot(kind='area', alpha=0.35, figsize=(20, 10)) 
+    plt.title('Immigration trend of top 5 countries')
+    plt.ylabel('Number of immigrants')
+    plt.xlabel('Years')
+*Option 2: Artist layer (Object oriented method) - using an Axes instance from Matplotlib (preferred) *
+
+You can use an Axes instance of your current plot and store it in a variable (eg. ax). You can add more elements by calling methods with a little change in syntax (by adding "set_" to the previous methods). For example, use ax.set_title() instead of plt.title() to add title, or ax.set_xlabel() instead of plt.xlabel() to add label to the x-axis.
+
+This option sometimes is more transparent and flexible to use for advanced plots (in particular when having multiple plots, as you will see later).
+
+In this course, we will stick to the scripting layer, except for some advanced visualizations where we will need to use the artist layer to manipulate advanced aspects of the plots."""
+
+# option 2: preferred option with more flexibility
+ax = df_top5.plot(kind='area', alpha=0.35, figsize=(20, 10))
+
+ax.set_title('Immigration Trend of Top 5 Countries')
+ax.set_ylabel('Number of Immigrants')
+ax.set_xlabel('Years')
+
+"""Question: Use the scripting layer to create a stacked area plot of the 5 countries that contributed the least to immigration to Canada from 1980 to 2013. Use a transparency value of 0.45"""
+# get the 5 countries with the least contribution
+df_least5 = df_can.tail(5)
+
+# transpose the dataframe
+df_least5 = df_least5[years].transpose() 
+df_least5.head()
+
+df_least5.index = df_least5.index.map(int) # let's change the index values of df_least5 to type integer for plotting
+df_least5.plot(kind='area', alpha=0.45, figsize=(20, 10)) 
+
+plt.title('Immigration Trend of 5 Countries with Least Contribution to Immigration')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+
+"""Question: Use the artist layer to create an unstacked area plot of the 5 countries that contributed the least to immigration to Canada from 1980 to 2013. Use a transparency value of 0.55."""
+# get the 5 countries with the least contribution
+df_least5 = df_can.tail(5)
+
+# transpose the dataframe
+df_least5 = df_least5[years].transpose() 
+df_least5.head()
+
+df_least5.index = df_least5.index.map(int) # let's change the index values of df_least5 to type integer for plotting
+
+ax = df_least5.plot(kind='area', alpha=0.55, stacked=False, figsize=(20, 10))
+
+ax.set_title('Immigration Trend of 5 Countries with Least Contribution to Immigration')
+ax.set_ylabel('Number of Immigrants')
+ax.set_xlabel('Years')
+
+"""Histograms
+A histogram is a way of representing the frequency distribution of numeric dataset. The way it works is it partitions the x-axis into bins, assigns each data point in our dataset to a bin, and then counts the number of data points that have been assigned to each bin. So the y-axis is the frequency or the number of data points in each bin. Note that we can change the bin size and usually one needs to tweak it so that the distribution is displayed nicely.
+
+
+Question: What is the frequency distribution of the number (population) of new immigrants from the various countries to Canada in 2013?
+
+Before we proceed with creating the histogram plot, let's first examine the data split into intervals. To do this, we will us Numpy's histrogram method to get the bin ranges and frequency counts as follows:"""
+# let's quickly view the 2013 data
+df_can['2013'].head()
+# np.histogram returns 2 values
+count, bin_edges = np.histogram(df_can['2013'])
+
+print(count) # frequency count
+print(bin_edges) # bin ranges, default = 10 bins
+
+"""By default, the histrogram method breaks up the dataset into 10 bins. The figure below summarizes the bin ranges and the frequency distribution of immigration in 2013. We can see that in 2013:
+
+178 countries contributed between 0 to 3412.9 immigrants
+11 countries contributed between 3412.9 to 6825.8 immigrants
+1 country contributed between 6285.8 to 10238.7 immigrants, and so on..
+Image
+We can easily graph this distribution by passing kind=hist to plot()."""
+df_can['2013'].plot(kind='hist', figsize=(8, 5))
+
+plt.title('Histogram of Immigration from 195 Countries in 2013') # add a title to the histogram
+plt.ylabel('Number of Countries') # add y-label
+plt.xlabel('Number of Immigrants') # add x-label
+
+plt.show()
+
+"""In the above plot, the x-axis represents the population range of immigrants in intervals of 3412.9. The y-axis represents the number of countries that contributed to the aforementioned population.
+
+Notice that the x-axis labels do not match with the bin size. This can be fixed by passing in a xticks keyword that contains the list of the bin sizes, as follows:"""
+# 'bin_edges' is a list of bin intervals
+count, bin_edges = np.histogram(df_can['2013'])
+
+df_can['2013'].plot(kind='hist', figsize=(8, 5), xticks=bin_edges)
+
+plt.title('Histogram of Immigration from 195 countries in 2013') # add a title to the histogram
+plt.ylabel('Number of Countries') # add y-label
+plt.xlabel('Number of Immigrants') # add x-label
+
+plt.show()
+
+"""Side Note: We could use df_can['2013'].plot.hist(), instead. In fact, throughout this lesson, using some_data.plot(kind='type_plot', ...) is equivalent to some_data.plot.type_plot(...). That is, passing the type of the plot as argument or method behaves the same.
+
+See the pandas documentation for more info http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.plot.html.
+
+We can also plot multiple histograms on the same plot. For example, let's try to answer the following questions using a histogram.
+
+Question: What is the immigration distribution for Denmark, Norway, and Sweden for years 1980 - 2013?"""
+# let's quickly view the dataset 
+df_can.loc[['Denmark', 'Norway', 'Sweden'], years]
+
+# generate histogram
+df_can.loc[['Denmark', 'Norway', 'Sweden'], years].plot.hist()
+
+"""That does not look right!
+
+Don't worry, you'll often come across situations like this when creating plots. The solution often lies in how the underlying dataset is structured.
+
+Instead of plotting the population frequency distribution of the population for the 3 countries, pandas instead plotted the population frequency distribution for the years.
+
+This can be easily fixed by first transposing the dataset, and then plotting as shown below."""
+
+# transpose dataframe
+df_t = df_can.loc[['Denmark', 'Norway', 'Sweden'], years].transpose()
+df_t.head()
+
+# generate histogram
+df_t.plot(kind='hist', figsize=(10, 6))
+
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants')
+
+plt.show()
+
+"""Let's make a few modifications to improve the impact and aesthetics of the previous plot:
+
+        increase the bin size to 15 by passing in bins parameter
+        set transparency to 60% by passing in alpha paramemter
+        label the x-axis by passing in x-label paramater
+        change the colors of the plots by passing in color parameter
+"""
+# let's get the x-tick values
+count, bin_edges = np.histogram(df_t, 15)
+
+# un-stacked histogram
+df_t.plot(kind ='hist', 
+          figsize=(10, 6),
+          bins=15,
+          alpha=0.6,
+          xticks=bin_edges,
+          color=['coral', 'darkslateblue', 'mediumseagreen']
+         )
+
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants')
+
+plt.show()
+
+
+
+
+
+
+
